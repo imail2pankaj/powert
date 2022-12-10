4fa1efac427f1c565cff41cadaff61864cde2ee2
pankaj <imail2pankaj@gmail.com>
2022-12-10 18:51:24 +0530

    Close mobile app on route change

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Container from '../containers/container'
+import { LayoutContext } from '../contexts/LayoutContext'
 import Navbar from './Navbar'
 
+
 const Layout = ({ children, bgClass }) => {
+  const router = useRouter()
+  const {toggleMenu, setToggleMenu} = useContext(LayoutContext);
+  useEffect(() => {
+    if(toggleMenu) setToggleMenu(false)
+  }, [router])
   return (
     <div className={`h-screen bg-primary text-white relative ${bgClass}`}>
       <Container>
