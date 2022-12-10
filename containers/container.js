7076ff9232429fb7d4cc26b1dc02ac550f8f9028
pankaj <imail2pankaj@gmail.com>
2022-12-10 18:22:59 +0530

    Some fixes

--- containers/container.js
+++ containers/container.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children}) => {
   return (
-    <div className='mx-auto w-full max-w-7xl px-4 lg:px-0'>
+    <div className='mx-auto max-w-7xl w-full px-4 lg:px-0'>
       {children}
     </div>
   )
